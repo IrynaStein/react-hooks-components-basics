2b04c315cc407b5eb24cf99099fcfa8159324724
Iryna Stein <isophiestein@gmail.com>
2021-06-07 21:01:42 -0600

    Lab code along

--- src/Article.js
+++ src/Article.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Article() {
   return (
-    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    <h3>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</h3>
   );
 }
 
